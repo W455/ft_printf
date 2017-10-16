e04976faeac1077074588287953217b7ec96209b
Ouassim KRIFA <oukrifa@estudent.42.fr>
2017-10-17 01:05:58 +0200

    put c

--- srcs/conversion/put/put_c.c
+++ srcs/conversion/put/put_c.c
@@ -1,34 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   put_c.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oukrifa <oukrifa@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/10/14 00:20:25 by oukrifa           #+#    #+#             */
+/*   Updated: 2017/10/14 01:26:27 by oukrifa          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "libftprintf.h"
 
-static void     put_space(int pos, t_flag *env)
+static	void	put_space(int call, t_flag *env)
 {
-    if (pos == 1)
+    if (call == 1)
     {
-        while (!FLAG('-') && !FLAG['0']) && 
-                env->width > PRECISION)
-                add_to_buff(env, ' ');
-        FLAG('-') ? 1 : PRECISION++;
-    }    
-    if (pos == 2)
-        while (FLAG['-'] && env->width >= 0)
+        while (!FLAG['-'] && !FLAG['0'] && WIDTH > 1)
             add_to_buff(env, ' ');
-}
-
-static void     put_value(t_flag *env, char c)
-{
-    while (env->width > 0)
-    {
-        add_to_buff(env, ' ');
-        env->width--;
     }
-    add_to_buff(env, c);
+    else if (call == 2)
+    {
+        while (FLAG['-'] && WIDTH >= 0)
+            add_to_buff(env, ' ');
+    }
 }
 
-void    put_c(char c, t_flag env)
+static	void	put_precision_or_0(t_flag *env)
 {
-    put_space(1, env);
-    put_value(env, c);
-    put_space(2, env);
+	while (WIDTH-- > 1 && FLAG['0'])
+		add_to_buff(env, '0');
+}
 
-}   
+void    put_c(char c, t_flag *env)
+{
+    WIDTH ? put_space(1, env) : 0;
+    put_precision_or_0(env);
+    add_to_buff(env, c);
+    WIDTH > 0 ? put_space(2, env) : 0;
+}
